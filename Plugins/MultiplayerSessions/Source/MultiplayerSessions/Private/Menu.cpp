5f586c62504eb062ade9a60105037f610fed135e
anywherey <2857579766@qq.com>
2025-05-20 19:55:10 +0800

    feat: 2.25. Path to Lobby

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -7,8 +7,9 @@
 #include "OnlineSessionSettings.h"
 #include "OnlineSubsystem.h"
 #include "Interfaces/OnlineSessionInterface.h"
-void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch)
+void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch,FString LobbyPath)
 {
+	PathToLobby = FString::Printf(TEXT("%s?listen?SeamlessTravel=0"), *LobbyPath);
 	NumPublicConnections = NumberOfPublicConnections;
 	MatchType = TypeOfMatch;
 
@@ -76,7 +77,7 @@ void UMenu::OnCreateSession(bool bWasSuccessful)
 		}
 		UWorld* World = GetWorld();
 		if (World) {
-			World->ServerTravel(FString("/Game/ThirdPerson/Maps/Lobby?listen?SeamlessTravel=0"));
+			World->ServerTravel(PathToLobby);
 		}
 	}
 	else {
